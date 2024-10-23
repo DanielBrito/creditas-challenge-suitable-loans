f779de91981746ac6b391d192088b0da8ced40b1
Daniel Brito <daniell-henrique@hotmail.com>
2024-10-23 19:17:15 -0300

    feat: enable actuator

--- loan/build.gradle.kts
+++ loan/build.gradle.kts
@@ -36,6 +36,7 @@ testSets {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
